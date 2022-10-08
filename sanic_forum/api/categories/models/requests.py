02c8dc661f373670d907aa0a87e7e05f83d93d77
Néstor Pérez <25409753+prryplatypus@users.noreply.github.com>
2022-10-08 21:07:58 +0200

    Fix category validation (#24)

--- sanic_forum/api/categories/models/requests.py
+++ sanic_forum/api/categories/models/requests.py
@@ -16,18 +16,20 @@ class CreateCategoryRequest(BaseModel):
     type: AllowedCategoryType
 
     @validator("display_order")
-    def validate_display_order(cls, display_order: int):
+    def validate_display_order(cls, display_order: int) -> int:
         min_ = 1
         if display_order < min_:
             raise BadRequest(
                 f"Display order must be an integer greater or equal to {min_}"
             )
+        return display_order
 
     @validator("name")
-    def validate_name(cls, name: str) -> None:
+    def validate_name(cls, name: str) -> str:
         name_len = len(name)
         min_, max_ = 1, 250
         if name_len < min_ or name_len > max_:
             raise BadRequest(
                 f"Name must be between {min_} and {max_} characters"
             )
+        return name
